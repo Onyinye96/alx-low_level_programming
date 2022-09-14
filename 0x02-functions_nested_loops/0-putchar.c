cbc702082bedf4fd5d46db2d2f8c59bbb51fa9d4
Onyinye96 <onyinyeveronica28@gmail.com>
2022-09-14 16:43:26 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,8 @@
 #include "main.h"
 
 /**
- * main - Entry point
- *
- * Return: Always returns 0
- *
+ * main - print putchar
+ * Return: always 0
  */
 
 int main(void)
@@ -18,5 +16,6 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
+
 	return (0);
 }

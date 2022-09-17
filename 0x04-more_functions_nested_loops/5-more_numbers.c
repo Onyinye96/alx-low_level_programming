5a042a75d153ac7850bba0f9dcd12c1550322fa7
Onyinye96 <onyinyeveronica28@gmail.com>
2022-09-17 10:40:54 -0700

    Task 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * more_numbers - Print number set 5 times
+ * Return: Nothing
+ */
+void more_numbers(void)
+{
+	int i = 0;
+	int j;
+
+	while (i < 10)
+	{
+		j = 0;
+		while (j <= 14)
+		{
+			if (j >= 10)
+			{
+				_putchar(j / 10 + '0');
+			}
+			_putchar(j % 10 + '0');
+			j++;
+		}
+		i++;
+		_putchar('\n');
+	}
+}
+
